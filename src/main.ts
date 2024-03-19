45aaf0960be9a6f98a9dbcdca7ab6d0565ac9dee
anoy-ch <anoy@eatlbd.com>
2024-03-19 23:56:07 +0600

    resolved cors issue

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('User API')
     .setDescription('API description for managing users')
